72dc455e5ac644c357c8711db952d43e13711f93
James <jameslimbj@gmail.com>
2021-02-08 13:39:23 +0800

    :construction: WIP: set up basic client logic

--- index.js
+++ index.js
@@ -3,3 +3,23 @@ const config = require("./config.json");
 
 const client = new Discord.Client();
 client.login(config.BOT_TOKEN);
+
+client.on("message", message => {
+    if (message.author.bot) return;
+
+    if (message.content.startsWith(config.PREFIX)) {
+        const body = message.content.slice(config.PREFIX.length);
+        const args = body.split(' ');
+        const cmd = args.shift().toLowerCase();
+
+        if (cmd == "vanish") {
+            const value = args.shift().toLowerCase();
+            if (value == "off") {
+
+            } else {
+                const t = Number.parseInt(value);
+                message.channel.send(`:cloud_tornado: Messages will disappear after ${t}s. ${message.createdTimestamp}`);
+            }
+        }
+    }
+});
