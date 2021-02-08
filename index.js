9aaf40ce774250910269f4f1fedc2d0b1d64d75f
James <jameslimbj@gmail.com>
2021-02-08 14:09:13 +0800

    :hammer: REFACTOR: separate handling of cmd and normal messages

--- index.js
+++ index.js
@@ -6,23 +6,36 @@ import Time from "./time";
 const client = new Discord.Client();
 client.login(config.BOT_TOKEN);
 
-client.on("message", (message) => {
-  if (message.author.bot) return;
+var channels;
 
-  if (message.content.startsWith(config.PREFIX)) {
-    const body = message.content.slice(config.PREFIX.length);
-    const args = body.split(" ");
-    const cmd = args.shift().toLowerCase();
-
-    if (cmd == "vanish") {
-      const value = args.shift().toLowerCase();
-      if (value == "off") {
-      } else {
-        const t = Time.parseDuration(value);
-        message.channel.send(
-          `:cloud_tornado: Messages will disappear after ${t}s. ${message.createdTimestamp}`
-        );
-      }
-    }
+const onVanish = (channel, value) => {
+  if (value == "off") {
+    channels[channel.id].duration = 0;
+  } else {
+    const t = Time.parseDuration(value);
+    channels[channel.id].duration = t;
+    channel.send(`:cloud_tornado: Messages will disappear after ${t}s.`);
   }
+};
+
+const cmdHandler = (message) => {
+	const body = message.content.slice(config.PREFIX.length);
+  const args = body.split(" ");
+  const cmd = args.shift().toLowerCase();
+
+  if (cmd == "vanish") {
+		onVanish(message.channel, args.shift().toLowerCase());
+  }
+};
+
+const vanishHandler = (message) => {
+
+};
+
+client.on("message", (message) => {
+  if (message.content.startsWith(config.PREFIX) && !message.author.bot) {
+    cmdHandler(message);
+  } else if (channels[message.channel.id].duration > 0) {
+    vanishHandler(message);
+	}
 });
